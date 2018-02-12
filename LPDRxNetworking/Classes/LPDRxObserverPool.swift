d58135bf54ef962591eb3cba3fe0916810127d2f
boyce <hepeilin1987@qq.com>
2018-02-12 15:32:29 +0800

    完善了ObserverPool的代码

--- LPDRxNetworking/Classes/LPDRxObserverPool.swift
+++ LPDRxNetworking/Classes/LPDRxObserverPool.swift
@@ -6,10 +6,74 @@
 //
 
 import Foundation
+import RxSwift
 
 class LPDRxObserverPool: NSObject {
   
+  
+  var poolDic:NSMutableDictionary = NSMutableDictionary()
+  let lock = NSLock()
+  
   static let sharedInstance = LPDRxObserverPool()
   
+  override init() {
+    super.init()
+    
+    self.register()
+  }
+  
+  
+  func register(){
+    
+    NotificationCenter.default.addObserver(self, selector:#selector(LPDRxObserverPool.handleReceiveMemory(notification:)) , name: Notification.Name.UIApplicationDidReceiveMemoryWarning, object: nil)
+    
+  }
+
+  @objc public func handleReceiveMemory(notification:NSNotification){
+    
+    self.lock.lock()
+    
+    for key in self.poolDic.allKeys {
+      
+      let table = self.poolDic.object(forKey: key) as? NSHashTable<AnyObject>
+      
+      if table?.count == 0{
+        self.poolDic.removeObject(forKey: key)
+      }
+      
+    }
+  
+    self.lock.unlock()
+    
+  }
+  
+  func addObserver(observer:Any,identifier:String){
+   
+    self.lock.lock()
+    var table = self.poolDic.value(forKey: identifier) as? NSHashTable<AnyObject>
+    
+    if table == nil {
+       table = NSHashTable()
+      self.poolDic.setObject(table!, forKey: NSString.init(string: identifier))
+    }
+    
+    if !(table?.contains(observer as AnyObject))! {
+      table?.add(observer as AnyObject)
+    }
+    
+    self.lock.unlock()
+    
+  }
+ 
+  
+  func allObserversIdentifier(_ identifier:String) -> Array<Any>?{
+    
+    self.lock.lock()
+    let table = self.poolDic.value(forKey: identifier) as? NSHashTable<AnyObject>
+    self.lock.unlock()
+  
+    return table?.allObjects
+   
+  }
 
 }
